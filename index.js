ff6b65b7dc57cc9bd0a7b0038c1a77040a4c678f
Pawan kumar <pawankumar639951@gmail.com>
2026-03-29 01:12:45 +0530

    feat: setup listing schema and initial database seeding

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const path = require("path")
+const mongoose = require('mongoose');
+const Listing = require("./models/listing.js");
+
+
+
+main().then((res) => {
+    console.log("connection successful");
+}).catch((err) => {
+    console.log(err);
+})
+
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended : true}))
+
+
+app.get("/", (req, res) => {
+    res.send("hello world from root node ");
+});
+
+// app.get("/listing", async(req,res) => {
+
+//     let samplelisting = new Listing({
+//         title : "Mathematics",
+//         description : "Written by RD sharma",
+//         price : 2000,
+//         location : "pune",
+//         country : "India"
+//     });
+//     await samplelisting.save()
+//     console.log("sample is saved");
+//     res.send("Succefull testing")
+// })
+
+app.get("/listings", async (req,res) => {
+    // res.send("working")
+    let alllistings = await Listing.find({})
+    res.render("./listings/index.ejs", {alllistings})
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+})
