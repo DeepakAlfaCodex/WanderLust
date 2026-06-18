492917e657226238717b17e72a67ff3c29066d44
DeepakAlfaCodex <ds2900050@gmail.com>
2026-06-18 15:18:22 +0530

    Image Preview for Edit Page

--- controllers/listing.js
+++ controllers/listing.js
@@ -32,7 +32,7 @@ module.exports.createListing = async (req, res) => {
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
-  newListing.image = {url, filename};
+  newListing.image = { url, filename };
   await newListing.save();
   req.flash("success", "New Listing Created");
   res.redirect("/listings");
@@ -45,12 +45,20 @@ module.exports.renderEditForm = async (req, res) => {
     req.flash("success", "Listing does not exists");
     res.redirect("/listings");
   }
-  res.render("./listings/edit.ejs", { listing });
+  let originalImageUrl = listing.image.url;
+  originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+  res.render("./listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { url, filename };
+    await listing.save();
+  }
   req.flash("success", "Listing updated");
   res.redirect(`/listings/${id}`);
 };
