6208837d8023f79ba973ee673cac8a09312f0480
DeepakAlfaCodex <ds2900050@gmail.com>
2026-06-18 14:29:20 +0530

    Configure environment variables and reintialized data

--- routes/listing.js
+++ routes/listing.js
@@ -4,12 +4,17 @@ const wrapAsync = require("../utills/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 // --- 1. Root Route ("/") ---
 // Isme Index aur Create dono ko ek sath chain kar diya
-router.route("/")
-    .get(wrapAsync(listingController.index))
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
+
 
 // --- 2. New Route ("/new") ---
 // Iska koi joda nahi hai, toh ise alag hi rakhna padega
@@ -17,18 +22,24 @@ router.get("/new", isLoggedIn, listingController.renderForm);
 
 // --- 3. ID Route ("/:id") ---
 // Isme Show, Update aur Delete teeno ko ek sath chain kar diya
-router.route("/:id")
-    .get(wrapAsync(listingController.showListing))
-    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
-    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing),
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 // --- 4. Edit Route ("/:id/edit") ---
 // Ye bhi unique URL hai, toh ise alag rakha hai
 router.get(
-    "/:id/edit",
-    isLoggedIn,
-    isOwner,
-    wrapAsync(listingController.renderEditForm)
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm),
 );
 
 module.exports = router;
